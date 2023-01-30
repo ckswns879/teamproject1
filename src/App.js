4c7c6cfaa9305ce5960b1655b1b3452ad4288a1c
ckswns879 <ckswns879@naver.com>
2023-01-30 09:24:59 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"; //ㅎㅇㅎㅇ
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Main from "./routes/Main";
